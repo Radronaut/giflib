edda78d7806cf163bdbed1eb06abf7ad07190183
Justin Carroll <JWCarroll88@gmail.com>
2019-06-23 16:37:46 -0500

    A little autowiring

--- src/main/java/com/teamtreehouse/giflib/controller/GifController.java
+++ src/main/java/com/teamtreehouse/giflib/controller/GifController.java
@@ -1,22 +1,28 @@
 package com.teamtreehouse.giflib.controller;
 
-import com.teamtreehouse.giflib.model.Gif;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+        import com.teamtreehouse.giflib.data.GifRepository;
+        import com.teamtreehouse.giflib.model.Gif;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.stereotype.Controller;
+        import org.springframework.ui.ModelMap;
+        import org.springframework.web.bind.annotation.RequestMapping;
+        import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.time.LocalDate;
+        import java.time.LocalDate;
 
 @Controller
 public class GifController {
+    @Autowired
+    private GifRepository gifRepository;
+
     @RequestMapping("/")
     public String listGifs() {
         return "home";
     }
     @RequestMapping("/gif")
     public String gifDetails(ModelMap modelMap) {
-        Gif gif = new Gif("compiler-bot", LocalDate.of(2015,2,13), "Hank Jones", true);
+
+        Gif gif = gifRepository.findByName("android-explosion");
         modelMap.put("gif", gif);
         return "gif-details";
     }
