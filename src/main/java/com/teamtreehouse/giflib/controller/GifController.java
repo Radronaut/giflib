f80cb5e7307f68fe86152f95ddd50a76706d1c7a
Justin Carroll <JWCarroll88@gmail.com>
2019-06-23 09:41:02 -0500

    Model Map implemented for details

--- src/main/java/com/teamtreehouse/giflib/controller/GifController.java
+++ src/main/java/com/teamtreehouse/giflib/controller/GifController.java
@@ -1,8 +1,12 @@
 package com.teamtreehouse.giflib.controller;
 
+import com.teamtreehouse.giflib.model.Gif;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalDate;
 
 @Controller
 public class GifController {
@@ -11,8 +15,9 @@ public class GifController {
         return "home";
     }
     @RequestMapping("/gif")
-    @ResponseBody
-    public String showGif() {
-        return "A single gif";
+    public String gifDetails(ModelMap modelMap) {
+        Gif gif = new Gif("compiler-bot", LocalDate.of(2015,2,13), "Hank Jones", true);
+        modelMap.put("gif", gif);
+        return "gif-details";
     }
 }
