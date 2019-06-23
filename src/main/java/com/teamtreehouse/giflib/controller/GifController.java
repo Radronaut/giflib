c2ecf694dc41ef3d844e1c082b06c225579c3f8d
Justin Carroll <JWCarroll88@gmail.com>
2019-06-23 09:15:00 -0500

    First Code Push

---
+++ src/main/java/com/teamtreehouse/giflib/controller/GifController.java
@@ -0,0 +1,18 @@
+package com.teamtreehouse.giflib.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class GifController {
+    @RequestMapping("/")
+    public String listGifs() {
+        return "home";
+    }
+    @RequestMapping("/gif")
+    @ResponseBody
+    public String showGif() {
+        return "A single gif";
+    }
+}
